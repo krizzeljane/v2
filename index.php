2ea7fe41a03a57a0fef55fa65d4686cb41d811b3
krizzeljane <kj.bigol@gmail.com>
2019-04-25 11:02:18 +0800

    wthru updates

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <div class="container-fluid wthru">
 	<h3>Walkthrough</h3>
 
+	<div class="wthru-row"><div class="label label-default">Internal</div>  <a target="_blank" href="https://github.com/krizzeljane/v2.git">Github Repository</a></div> <br>
+	<div class="wthru-row"><div class="label label-default">Internal</div>  <a target="_blank" href="style.php">V2 Style Guide</a></div> <br>
+
 	<br>
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="index-2.php">Homepage</a></div> <br>
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="home.php">Landing Page</a></div> <br>
@@ -10,9 +13,8 @@
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="carsisold/carsisold.php">Cars I Sold</a></div> <br>
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="users/dealers.php">Dealers Page</a> </div><br>
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="users/userprofile.php">User Profile</a> | <a target="_blank" href="users/useraccount.php">Your User Profile</a></div> <br>
-	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="users/userprofile.php">User Profile</a> | <a target="_blank" href="users/useraccount.php">Your User Profile</a></div> <br>
 	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="buybid/buybid.php">Get Buy Bid</a></div> <br>
-	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="vehicleinfo/vehicleinfo.php">Vehicle Info - Frontline</a> | <a target="_blank" href="vehicleinfo/vehicleinfo-ft.php">Vehicle Info - Trade-ins</a></div> <br>
+	<div class="wthru-row"><div class="label label-primary">User</div>  <a target="_blank" href="vehicle-info/vehicleinfo.php">Vehicle Info - Frontline</a> | <a target="_blank" href="vehicleinfo/vehicle-info-ft.php">Vehicle Info - Trade-ins</a></div> <br>
 
 
 	<div class="wthru-row"><div class="label label-danger">Admin</div>  <a target="_blank" href="pre-sale/preauction.php">Quality Control</a></div> <br>
@@ -20,6 +22,7 @@
 	<div class="wthru-row"><div class="label label-danger">Admin</div>  <a target="_blank" href="carsibought/carsibought.php">Cars I Bought</a></div> <br>
 	<div class="wthru-row"><div class="label label-danger">Admin</div>  <a target="_blank" href="carsisold/carsisold.php">Cars I Sold</a> </div><br>
 	<div class="wthru-row"><div class="label label-danger">Admin</div>  <a target="_blank" href="users/dealers.php">Dealers Page</a></div> <br>
+	<div class="wthru-row"><div class="label label-danger">Admin</div>  <a target="_blank" href="users/accounts.php">Accounts Page</a></div> <br>
 	
 
 </div>
